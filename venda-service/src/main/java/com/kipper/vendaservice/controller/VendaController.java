ea9fa9948e1df9eee9de9d8bc45fdaed6fb48605
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-11-30 14:27:40 -0300

    feature: finalizando comunicação entre os microsservicos

--- venda-service/src/main/java/com/kipper/vendaservice/controller/VendaController.java
+++ venda-service/src/main/java/com/kipper/vendaservice/controller/VendaController.java
@@ -3,6 +3,7 @@ package com.kipper.vendaservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class VendaController {
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 	
+	@PostMapping
 	public ResponseEntity<Void> realizarVenda(@RequestBody String idProduto){
 		this.kafkaTemplate.send("estoque-topico", idProduto);
 		return ResponseEntity.ok().build();
