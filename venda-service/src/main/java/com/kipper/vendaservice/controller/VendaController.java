22a69a308244f887837b8bb3bc0d0663c676dd33
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-11-30 13:14:48 -0300

    feature: configurando serviço de venda para enviar a mensagem para o tópico de estoque no tópico do kafka

--- venda-service/src/main/java/com/kipper/vendaservice/controller/VendaController.java
+++ venda-service/src/main/java/com/kipper/vendaservice/controller/VendaController.java
@@ -1,5 +1,22 @@
 package com.kipper.vendaservice.controller;
 
-public class VendaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/venda")
+public class VendaController {
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	public ResponseEntity<Void> realizarVenda(@RequestBody String idProduto){
+		this.kafkaTemplate.send("estoque-topico", idProduto);
+		return ResponseEntity.ok().build();
+	}
+	
 }
